a4e001a64a080b95e012828ff5ead8a7dd440693
Josh Ramos <josh.r.938@gmail.com>
2019-06-03 13:41:05 -0500

    removed the int value on line 7

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT unsigned NOT NULL auto_increment,
 	artist varchar(255) NOT NULL,
 	name varchar(255) NOT NULL,
-	release_date INT(1998) NOT NULL,
+	release_date INT NOT NULL,
 	sales FLOAT NOT NULL,
 	genre varchar(255) NOT NULL,
 	primary key (id)
