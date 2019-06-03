4b31512f2487ead0a8f7b6d10be24481bdf32817
Josh Ramos <josh.r.938@gmail.com>
2019-06-03 13:34:03 -0500

    This is the finished MySQL tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE if exists albums;
+CREATE TABLE albums (
+    id INT unsigned NOT NULL auto_increment;
+	artist varchar(255) NOT NULL;
+	name varchar(255) NOT NULL;
+	release_date INT(1998) NOT NULL;
+	sales FLOAT NOT NULL;
+	genre varchar(255) NOT NULL;
+	primary key (id)
+);
