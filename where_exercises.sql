e3641d669a0743f58bb44b1270f24d6a0f71ca4a
Josh Ramos <josh.r.938@gmail.com>
2019-06-04 15:41:30 -0500

    This is the finished MySQL Where exercises V2.0

--- where_exercises.sql
+++ where_exercises.sql
@@ -20,3 +20,24 @@ SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
 
+# ------------------------------------------------------------
+
+SELECT * FROM employees
+WHERE (first_name = 'Irena' OR
+      first_name = 'Vidya' OR
+      first_name = 'Maya')
+AND gender = 'M';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25'
+AND hire_date LIKE '199%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name
+NOT LIKE '%qu%';
